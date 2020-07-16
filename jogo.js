797e42aa817c2260a759f50da852e75bde9bfac3
MatheusFilipe21 <matheusfilipe1999@gmail.com>
2020-07-16 20:50:26 -0300

    Implementando tamanhos para o mosquito, alternando o lado de exibição do mesmo

--- jogo.js
+++ jogo.js
@@ -10,18 +10,55 @@ function ajustaTamanhoDoNavegador(){
 ajustaTamanhoDoNavegador()
 
 // Gerando um posição aleatória na tela sem exceder o limite da página considerando o tamanho do mosquito
-var posicaoX = parseInt(Math.random() * largura) - 90
-var posicaoY = parseInt(Math.random() * altura) - 90
+function posicaoRandomica(){
 
-posicaoX = posicaoX < 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY
+    // Remover o mosquito anterior caso exista
+    if (document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()  
+    }
 
-// Criando o mosquito no html
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+    var posicaoX = parseInt(Math.random() * largura) - 90
+    var posicaoY = parseInt(Math.random() * altura) - 90
 
-document.body.appendChild(mosquito)
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    // Criando o mosquito no html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    document.body.appendChild(mosquito)
+
+}
+
+// Responsável pelo mosquito mudar de tamanho
+function tamanhoAleatorio(){
+
+    var classe = parseInt(Math.random() * 3)
+
+    switch (classe){
+        case 0:
+            return 'mosquito'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+// Responsável por mudar o lado do mosquito
+function ladoAleatorio(){
+    var lado = parseInt(Math.random() * 2)
+
+    switch (lado){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
